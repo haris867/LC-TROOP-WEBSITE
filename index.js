47b740a35382c7c1a0b22d55b7f8ca2865cc678c
Haris <haris.usman@hotmail.com>
2023-04-08 04:18:55 +0200

    Added new arrow SVG and tweaked animations

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const comingSoon = document.querySelector("#coming-soon");
 const stayTuned = document.querySelector("#stay-tuned");
-const arrow = document.querySelector("#arrow");
+const arrow = document.querySelector(".arrow");
+const logo = document.querySelector("#logo-svg path");
 
-comingSoon.style.display = "none";
-stayTuned.style.display = "none";
-arrow.style.display = "none";
+comingSoon.style.opacity = "0";
+stayTuned.style.opacity = "0";
+arrow.style.opacity = "0";
 
 anime({
-  targets: "#logo-svg path",
+  targets: logo,
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "easeInOutSine",
   duration: 2200,
@@ -16,16 +17,13 @@ anime({
   loop: false,
   complete: () => {
     anime({
-      targets: "path",
+      targets: "#logo-svg path",
       fill: "#ffffff",
       duration: 1500,
       easing: "easeInOutQuad",
       complete: () => {
-        comingSoon.style.display = "block";
-        stayTuned.style.display = "block";
-        arrow.style.display = "block";
         anime({
-          targets: comingSoon.querySelectorAll("span"),
+          targets: comingSoon,
           opacity: [0, 1],
           translateY: [20, 0],
           duration: 1500,
@@ -35,8 +33,8 @@ anime({
           targets: stayTuned,
           opacity: [0, 1],
           translateY: [20, 0],
-          duration: 500,
-          delay: 200,
+          duration: 1500,
+          delay: 1200,
           easing: "easeInOutQuad",
         });
         anime({
@@ -44,10 +42,55 @@ anime({
           opacity: [0, 1],
           translateY: [20, 0],
           duration: 500,
-          delay: 400,
+          delay: 2400,
           easing: "easeInOutQuad",
         });
       },
     });
   },
 });
+
+// Tried to make it rerun animation onclick
+
+// anime({
+//   targets: "#logo-svg path",
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: "easeInOutSine",
+//   duration: 2200,
+//   delay: 500,
+//   direction: "alternate",
+//   loop: false,
+//   complete: () => {
+//     anime({
+//       targets: "path",
+//       fill: "#ffffff",
+//       duration: 1500,
+//       easing: "easeInOutQuad",
+//     });
+//   },
+// });
+
+// const logo = document.querySelector("#logo-svg path");
+
+// function runAnimation() {
+//   logo.style.fill = "none";
+//   anime({
+//     targets: "#logo-svg",
+//     strokeDashoffset: [anime.setDashoffset, 0],
+//     easing: "easeInOutSine",
+//     duration: 2200,
+//     delay: 500,
+//     direction: "alternate",
+//     loop: false,
+//     complete: () => {
+//       anime({
+//         targets: "path",
+//         fill: "#ffffff",
+//         duration: 1500,
+//         easing: "easeInOutQuad",
+//       });
+//     },
+//   });
+// }
+
+// logo.addEventListener("click", runAnimation);
